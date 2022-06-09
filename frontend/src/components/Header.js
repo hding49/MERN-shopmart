4542cd0383c60c09359c9227e21c08d6cf4eef37
hding49 <hding49@uwo.ca>
2022-06-09 15:02:51 -0400

    fetch data from backend using get request

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
